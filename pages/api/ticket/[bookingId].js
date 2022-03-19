bde0b8b1091c0fd0b5261fe771b314c42c0408b8
dksaha <dhriti@klinify.com>
2022-03-19 12:17:17 +0530

    new init commit

---
+++ pages/api/ticket/[bookingId].js
@@ -0,0 +1,46 @@
+import nextConnect from 'next-connect';
+import middleware from '../../../middleware/database';
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+    const { bookingId } = req.query
+    console.log(bookingId);
+    let doc = await req.db.collection('ticket').findOne({
+        bookingId: { $eq: bookingId }
+    });
+    console.log(doc);
+    res.json(doc);
+});
+
+handler.post(async (req, res) => {
+    const { bookingId } = req.query
+    console.log(bookingId);
+    const body = JSON.parse(req.body)
+
+    if(body.admit) {
+        let doc = await req.db.collection('ticket').updateOne({
+            bookingId: { $eq: bookingId }
+        },{ $set: { admitted: true } });
+        console.log(doc);
+        res.json(doc);
+    }
+});
+
+handler.put(async (req, res) => {
+    const { bookingId } = req.query
+    console.log(bookingId);
+
+    const body = JSON.parse(req.body)
+    let doc = await req.db.collection('ticket').insertOne({
+        bookingId: body.bookingId,
+        seats: body.seats,
+        admitted: false
+    });
+    console.log(doc);
+    res.json(doc);
+});
+
+export default handler;
